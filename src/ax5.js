f91a683e953662255d673b98c081756b18a76b35
ThomasJ <tom@axisj.com>
2014-12-04 00:40:19 +0900

    2014.12.04 querySelectAll, querySelect polymer 코드 붙이고 테스트

--- src/ax5.js
+++ src/ax5.js
@@ -61,62 +61,63 @@ argument
 		}());
 	}
 
-	// Production steps of ECMA-262, Edition 5, 15.4.4.18
-	// Reference: http://es5.github.io/#x15.4.4.18
+	// ES5 15.4.4.18 Array.prototype.forEach ( callbackfn [ , thisArg ] )
+	// From https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
 	if (!Array.prototype.forEach) {
+		Array.prototype.forEach = function (fun /*, thisp */) {
+			if (this === void 0 || this === null) { throw TypeError(); }
 
-		Array.prototype.forEach = function(callback, thisArg) {
+			var t = Object(this);
+			var len = t.length >>> 0;
+			if (typeof fun !== "function") { throw TypeError(); }
 
-			var T, k;
-
-			if (this == null) {
-				throw new TypeError(' this is null or not defined');
-			}
-
-			// 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-			var O = Object(this);
-
-			// 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-			// 3. Let len be ToUint32(lenValue).
-			var len = O.length >>> 0;
-
-			// 4. If IsCallable(callback) is false, throw a TypeError exception.
-			// See: http://es5.github.com/#x9.11
-			if (typeof callback !== "function") {
-				throw new TypeError(callback + ' is not a function');
-			}
-
-			// 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-			if (arguments.length > 1) {
-				T = thisArg;
-			}
-
-			// 6. Let k be 0
-			k = 0;
-
-			// 7. Repeat, while k < len
-			while (k < len) {
-
-				var kValue;
-
-				// a. Let Pk be ToString(k).
-				//   This is implicit for LHS operands of the in operator
-				// b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-				//   This step can be combined with c
-				// c. If kPresent is true, then
-				if (k in O) {
-
-					// i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-					kValue = O[k];
-
-					// ii. Call the Call internal method of callback with T as the this value and
-					// argument list containing kValue, k, and O.
-					callback.call(T, kValue, k, O);
+			var thisp = arguments[1], i;
+			for (i = 0; i < len; i++) {
+				if (i in t) {
+					fun.call(thisp, t[i], i, t);
 				}
-				// d. Increase k by 1.
-				k++;
 			}
-			// 8. return undefined
+		};
+	}
+
+	// Document.querySelectorAll method
+	// http://ajaxian.com/archives/creating-a-queryselector-for-ie-that-runs-at-native-speed
+	// Needed for: IE7-
+	if (!document.querySelectorAll) {
+		document.querySelectorAll = function(selectors) {
+			var style = document.createElement('style'), elements = [], element;
+			document.documentElement.firstChild.appendChild(style);
+			document._qsa = [];
+
+			style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+			window.scrollBy(0, 0);
+			style.parentNode.removeChild(style);
+
+			while (document._qsa.length) {
+				element = document._qsa.shift();
+				element.style.removeAttribute('x-qsa');
+				elements.push(element);
+			}
+			document._qsa = null;
+			return elements;
+		};
+	}
+
+	// Document.querySelector method
+	// Needed for: IE7-
+	if (!document.querySelector) {
+		document.querySelector = function(selectors) {
+			var elements = document.querySelectorAll(selectors);
+			return (elements.length) ? elements[0] : null;
+		};
+	}
+
+	// Document.getElementsByClassName method
+	// Needed for: IE8-
+	if (!document.getElementsByClassName) {
+		document.getElementsByClassName = function(classNames) {
+			classNames = String(classNames).replace(/^|\s+/g, '.');
+			return document.querySelectorAll(classNames);
 		};
 	}
 
@@ -265,6 +266,7 @@ argument
 		}
 
 		// In addition to using the http://underscorejs.org : map, reduce, reduce_right, find
+		// todo : map, reduce 등등 구현
 		function map(O, _fn){
 
 		}
@@ -609,6 +611,7 @@ argument
  * Refer to this by {@link ax5}.
  * @namespace ax5.dom
  */
+	// todo : querySelectAll 을 활용한 dom 구현
 	ax5.dom = (function(){
 		return function(select){
 			console.log(select);
