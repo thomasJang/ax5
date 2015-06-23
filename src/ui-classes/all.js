d368f4beeb8a3b50109eed7c48d41c023c33cad0
ThomasJ <tom@axisj.com>
2015-06-24 01:08:22 +0900

    modal 완료.

--- src/ui-classes/all.js
+++ src/ui-classes/all.js
@@ -1,6 +1,6 @@
 /*
  * ax5 - v0.0.1 
- * 2015-06-23 
+ * 2015-06-24 
  * www.axisj.com Javascript UI Library
  * 
  * Copyright 2013, 2015 AXISJ.com and other contributors 
@@ -1321,12 +1321,16 @@
 				},
 				theme: 'default',
 				width: 300,
+				height: 400,
 				title: '',
-				msg: ''
+				heading: {
+					height: 30
+				}
 			};
 		}).apply(this, arguments);
 
 		this.active_modal = null;
+		this.els = {};
 		this.mask = new ax5.ui.mask();
 
 		var cfg = this.config;
@@ -1341,114 +1345,8 @@
 		 */
 			//== class body start
 		this.init = function(){
-			// after set_config();
 			cfg.id = 'ax5-modal-' + ax5.get_guid();
-
 			this.mask.set_config(cfg.mask);
-		};
-
-		/**
-		 * open the modal of alert type
-		 * @method ax5.ui.modal.alert
-		 * @param {Object|String} [{theme, title, msg, btns}|msg] - modal 속성을 json으로 정의하거나 msg만 전달
-		 * @param {Function} [callback] - 사용자 확인 이벤트시 호출될 callback 함수
-		 * @returns {ax5.ui.modal}
-		 * @example
-		 * ```
-		 * my_modal.alert({
-		 *  title: 'app title',
-		 *  msg: 'alert'
-		 * }, function(){});
-		 * ```
-		 */
-		this.alert = function(opts, callback) {
-			if(U.is_string(opts)){
-				opts = {
-					title: cfg.title,
-					msg: opts
-				}
-			}
-			opts.modal_type = "alert";
-			opts.theme = (opts.theme || cfg.theme || "");
-			if(typeof opts.btns === "undefined"){
-				opts.btns = {
-					ok: {label: 'ok', theme: opts.theme}
-				};
-			}
-			this.open(opts, callback);
-			return this;
-		};
-
-		/**
-		 * open the modal of confirm type
-		 * @method ax5.ui.modal.confirm
-		 * @param {Object|String} [{theme, title, msg, btns}|msg] - modal 속성을 json으로 정의하거나 msg만 전달
-		 * @param {Function} [callback] - 사용자 확인 이벤트시 호출될 callback 함수
-		 * @returns {ax5.ui.modal}
-		 * @example
-		 * ```
-		 * my_modal.confirm({
-		 *  title: 'app title',
-		 *  msg: 'confirm'
-		 * }, function(){});
-		 * ```
-		 */
-		this.confirm = function(opts, callback) {
-			if(U.is_string(opts)){
-				opts = {
-					title: cfg.title,
-					msg: opts
-				}
-			}
-			opts.modal_type = "confirm";
-			opts.theme = (opts.theme || cfg.theme || "");
-			if(typeof opts.btns === "undefined"){
-				opts.btns = {
-					ok: {label: 'ok', theme: opts.theme},
-					cancel: {label: 'cancel'}
-				};
-			}
-			this.open(opts, callback);
-			return this;
-		};
-
-		/**
-		 * open the modal of prompt type
-		 * @method ax5.ui.modal.prompt
-		 * @param {Object|String} [{theme, title, msg, btns, input}|msg] - modal 속성을 json으로 정의하거나 msg만 전달
-		 * @param {Function} [callback] - 사용자 확인 이벤트시 호출될 callback 함수
-		 * @returns {ax5.ui.modal}
-		 * @example
-		 * ```
-		 * my_modal.prompt({
-		 *  title: 'app title',
-		 *  msg: 'alert'
-		 * }, function(){});
-		 * ```
-		 */
-		this.prompt = function(opts, callback) {
-			if(U.is_string(opts)){
-				opts = {
-					title: cfg.title,
-					msg: opts
-				}
-			}
-			opts.modal_type = "prompt";
-			opts.theme = (opts.theme || cfg.theme || "");
-
-			if(typeof opts.input === "undefined"){
-				opts.input = {
-					value: {label: (opts.msg || cfg.msg || "")}
-				};
-			}
-			if(typeof opts.btns === "undefined"){
-				opts.btns = {
-					ok: {label: 'ok', theme: opts.theme},
-					cancel: {label: 'cancel'}
-				};
-			}
-			this.open(opts, callback);
-			return this;
 		};
 
 
@@ -1456,38 +1354,25 @@
 			var
 				po = [];
 
-			po.push('<div id="' + modal_id + '" data-ax5-ui="modal" class="ax5-ui-modal ' + opts.theme + '">');
-				po.push('<div class="ax-modal-heading">');
+			po.push('<div id="' + modal_id + '" data-modal-els="root" class="ax5-ui-modal ' + opts.theme + '">');
+				po.push('<div class="ax-modal-heading" data-modal-els="heading" style="height:' + cfg.heading.height + 'px;line-height:' + cfg.heading.height + 'px;">');
 				po.push( (opts.title || cfg.title || "") );
 				po.push('</div>');
-				po.push('<div class="ax-modal-body">');
-					po.push('<div class="ax-modal-msg">');
-					if(opts.input){
-						U.each(opts.input, function(k, v) {
-							po.push('<div class="ax-modal-prompt">');
-							po.push( this.label.replace(/\n/g, "<br/>") );
-							po.push('</div>');
-							po.push('<input type="text" class="ax-inp" data-ax-modal-prompt="' + k + '" style="width:100%;" />');
-						});
+				po.push('<div class="ax-modal-body" data-modal-els="body">');
+					po.push('<iframe name="' + modal_id + '-frame" src="" width="100%" height="100%" frameborder="0" data-modal-els="iframe"></iframe>');
+					po.push('<form name="' + modal_id + '-form" data-modal-els="iframe-form">');
+					po.push('<input type="hidden" name="modal_id" value="' + modal_id + '" />');
+					for(var p in opts.http.param){
+						po.push('<input type="hidden" name="' + p + '" value="' + opts.http.param[p] + '" />');
 					}
-					else{
-						po.push( (opts.msg || cfg.msg || "").replace(/\n/g, "<br/>") );
-					}
-
-					po.push('</div>');
-					po.push('<div class="ax-modal-buttons">');
-						po.push('<div class="ax-button-wrap">');
-						U.each(opts.btns, function(k, v){
-							po.push('<button type="button" data-ax-modal-btn="' + k + '" class="ax-btn ' + this.theme + '">' + this.label + '</button>');
-						});
-						po.push('</div>');
-					po.push('</div>');
+					po.push('</form>');
 				po.push('</div>');
+
 			po.push('</div>');
 			return po.join('');
 		};
 
-		this.open = function(opts, callback){
+		this.open = function(opts){
 			var
 				pos = {},
 				box = {},
@@ -1496,115 +1381,58 @@
 			opts.id = (opts.id || cfg.id);
 
 			this.mask.open();
+
 			box = {
-				width: opts.width || cfg.width
+				width: opts.width || cfg.width,
+				height: opts.height || cfg.height
 			};
 			axd.append(document.body, this.get_content(opts.id, opts));
-			this.active_modal = ax5.dom('#' + opts.id);
-			this.active_modal.css({width: box.width});
 
-			// modal 높이 구하기 - 너비가 정해지면 높이가 변경 될 것.
-			box.height = this.active_modal.height();
+			this.active_modal = ax5.dom('#' + opts.id);
+			// 파트수집
+			this.els = {
+				"root": this.active_modal.find('[data-modal-els="root"]'),
+				"heading": this.active_modal.find('[data-modal-els="heading"]'),
+				"body": this.active_modal.find('[data-modal-els="body"]'),
+				"iframe": this.active_modal.find('[data-modal-els="iframe"]'),
+				"iframe-form": this.active_modal.find('[data-modal-els="iframe-form"]')
+			};
+
 			//- position 정렬
 			if(typeof opts.position === "undefined" || opts.position === "center"){
-				pos.top = ax5.dom.height(document.body) / 2 - box.height/2;
-				pos.left = ax5.dom.width(document.body) / 2 - box.width/2;
+				box.top = ax5.dom.height(document.body) / 2 - box.height/2;
+				box.left = ax5.dom.width(document.body) / 2 - box.width/2;
 			}else{
-				pos.left = opts.position.left || 0;
-				pos.top = opts.position.top || 0;
+				box.left = opts.position.left || 0;
+				box.top = opts.position.top || 0;
 			}
-			this.active_modal.css(pos);
+			this.active_modal.css(box);
 
-			// bind button event
-			if(opts.modal_type === "prompt") {
-				this.active_modal.find("[data-ax-modal-prompt]").elements[0].focus();
-			}else{
-				this.active_modal.find("[data-ax-modal-btn]").elements[0].focus();
-			}
-			this.active_modal.find("[data-ax-modal-btn]").on(cfg.click_event_name, (function(e){
-				this.btn_onclick(e||window.event, opts, callback);
+			this.els["iframe"].css({height: box.height-cfg.heading.height });
+
+
+			// iframe content load
+			this.els["iframe-form"].attr({"method": opts.http.method});
+			this.els["iframe-form"].attr({"target": opts.id + "-frame"});
+			this.els["iframe-form"].attr({"action": opts.http.url});
+			this.els["iframe"].on("load", (function(){
+				if(opts.onload) opts.onload.call(opts);
+				else if(cfg.onload) cfg.onload.call(opts);
 			}).bind(this));
-			
+			this.els["iframe-form"].elements[0].submit();
+
+
 			// bind key event
 			axd(window).on("keydown.ax-modal", (function(e){
-				this.onkeyup(e||window.event, opts, callback);
+				this.onkeyup(e||window.event, opts);
 			}).bind(this))
 		};
-
-		this.btn_onclick = function(e, opts, callback, target, k){
-			target = axd.parent(e.target, function(target){
-				if(ax5.dom.attr(target, "data-ax-modal-btn")){
-					return true;
-				}
-			});
-			if(target){
-				k = axd.attr(target, "data-ax-modal-btn");
-
-				var that = {
-						key: k, value: opts.btns[k],
-						modal_id: opts.id,
-						btn_target: target
-					};
-				if(opts.modal_type === "prompt") {
-					var empty_key = null;
-					for (var oi in opts.input) {
-						that[oi] = this.active_modal.find('[data-ax-modal-prompt=' + oi + ']').val();
-						if(that[oi] == "" || that[oi] == null){
-							empty_key = oi;
-							break;
-						}
-					}
-				}
-				if(opts.btns[k].onclick){
-					opts.btns[k].onclick.call(that, k);
-				}
-				else
-				if(opts.modal_type === "alert"){
-					if(callback) callback.call(that, k);
-					this.close();
-				}
-				else
-				if(opts.modal_type === "confirm"){
-					if(callback) callback.call(that, k);
-					this.close();
-				}
-				else
-				if(opts.modal_type === "prompt"){
-					if(k === 'ok') {
-						if(empty_key) {
-							this.active_modal.find('[data-ax-modal-prompt="' + empty_key + '"]').elements[0].focus();
-							return false;
-						}
-					}
-					if(callback) callback.call(that, k);
-					this.close();
-				}
-			}
-		};
 		
-		this.onkeyup = function(e, opts, callback, target, k){
+		this.onkeyup = function(e, opts, target, k){
 			if(e.keyCode == ax5.info.event_keys.ESC){
 				this.close();
-			}
-			if(opts.modal_type === "prompt") {
-				if(e.keyCode == ax5.info.event_keys.RETURN){
-					var that = {
-						key: k, value: opts.btns[k],
-						modal_id: opts.id,
-						btn_target: target
-					};
-					var empty_key = null;
-					for (var oi in opts.input) {
-						that[oi] = this.active_modal.find('[data-ax-modal-prompt=' + oi + ']').val();
-						if(that[oi] == "" || that[oi] == null){
-							empty_key = oi;
-							break;
-						}
-					}
-					if(empty_key) return false;
-					if(callback) callback.call(that, k);
-					this.close();
-				}
+				if(opts.onclose) opts.onclose.call(opts);
+				else if(cfg.onclose) cfg.onclose.call(opts);
 			}
 		};
 
